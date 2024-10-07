d875bc9e2fe9c4a3db40d87bb694cbdfc9e73253
brayanLopez <balg.lg55@gmail.com>
2024-10-07 10:34:42 -0500

    ADD colors for button

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -1,16 +1,27 @@
 import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-btn',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './btn.component.html',
   styleUrl: './btn.component.scss',
 })
 export class BtnComponent {
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
+  @Input() color: string = 'primary';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  get colors() {
+    const colorsList: { [key: string]: string } = {
+      success: 'bg-success-700 hover:bg-success-800 focus:ring-success-300',
+      danger: 'bg-red-700 hover:bg-red-800 focus:ring-red-300',
+      primary: 'bg-primary-700 hover:bg-primary-800 focus:ring-primary-300',
+    };
+    return colorsList[this.color];
+  }
 }
