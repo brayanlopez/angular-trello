f3a2e9da2937407629b41846d5372a58deec5c95
brayanLopez <balg.lg55@gmail.com>
2024-10-07 20:31:48 -0500

    ADD todo-dialog component

---
+++ src/app/components/todo-dialog/todo-dialog.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { DialogRef } from '@angular/cdk/dialog';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faClose,
+  faCheckToSlot,
+  faBars,
+  faUser,
+  faTag,
+  faCheckSquare,
+  faClock,
+} from '@fortawesome/free-solid-svg-icons';
+import { BtnComponent } from '../btn/btn.component';
+
+@Component({
+  selector: 'app-todo-dialog',
+  standalone: true,
+  imports: [BtnComponent, FontAwesomeModule],
+  templateUrl: './todo-dialog.component.html',
+  styleUrl: './todo-dialog.component.scss',
+})
+export class TodoDialogComponent {
+  faClose = faClose;
+  faCheckToSlot = faCheckToSlot;
+  faBars = faBars;
+  faUser = faUser;
+  faTag = faTag;
+  faCheckSquare = faCheckSquare;
+  faClock = faClock;
+
+  constructor(private dialogRef: DialogRef) {}
+
+  ngOnInit(): void {}
+
+  close() {
+    this.dialogRef.close();
+  }
+}
--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -7,14 +7,22 @@ import {
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
+import { Dialog, DialogModule } from '@angular/cdk/dialog';
 
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { Column, ToDo } from '../../models/todo.model';
+import { TodoDialogComponent } from '../../components/todo-dialog/todo-dialog.component';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [NavbarComponent, CdkDropList, CdkDropListGroup, CdkDrag],
+  imports: [
+    NavbarComponent,
+    CdkDropList,
+    CdkDropListGroup,
+    CdkDrag,
+    DialogModule,
+  ],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss',
 })
@@ -57,7 +65,7 @@ export class BoardComponent {
   doing: ToDo[] = [];
   done: ToDo[] = [];
 
-  constructor() {}
+  constructor(private dialog: Dialog) {}
 
   ngOnInit(): void {}
 
@@ -82,6 +90,13 @@ export class BoardComponent {
     this.columns.push({
       title: 'New Column',
       todos: [],
+    });
+  }
+
+  openDialog() {
+    this.dialog.open(TodoDialogComponent, {
+      minWidth: '300px',
+      maxWidth: '50%',
     });
   }
 }
